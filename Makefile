f1facb4454b27e9b8828667d8ca1b6b8aaa4ef66
Imam Alam <imam.alam@gmail.com>
2021-07-06 14:02:01 +0000

    more filters

--- Makefile
+++ Makefile
@@ -13,6 +13,9 @@ delete:
 list:
 	kubectl get pods -n processing
 
+logs:
+	kubectl logs -n processing -l app=dea-podsweeper
+
 save:
 	git commit -a -m meaningless-message
 	git push
--- podsweeper/__main__.py
+++ podsweeper/__main__.py
@@ -1,17 +1,67 @@
-# inspired by https://stackoverflow.com/questions/64221992/simple-way-to-delete-existing-pods-from-python/64222735#64222735
+from datetime import datetime, timezone
 
 import click
 from kubernetes import client, config
 
 @click.command()
-@click.option('--namespace', required=True)
-def main(namespace):
+@click.option('--namespace', required=True, type=str)
+@click.option('--field-selector', type=str)
+@click.option('--label-selector', type=str)
+@click.option('--pod-name-prefix', type=str, help='only select pods whose name starts with this prefix')
+@click.option('--expire-in-hours', type=int, help='only select pods that are aged more than this many hours')
+def main(namespace, field_selector, label_selector, pod_name_prefix, expire_in_hours):
     config.load_incluster_config()
 
+    kwargs = {'field_selector': field_selector, 'label_selector': label_selector}
+    kwargs = {key: value for key, value in kwargs.items() if value is not None}
+
     v1 = client.CoreV1Api()
-    print("Listing pods with their IPs:")
-    pods = v1.list_namespaced_pod(namespace=namespace, watch=False)
-    for p in pods.items:
-        print("%s\t%s\t%s" % (p.status.pod_ip, p.metadata.namespace, p.metadata.name))
+    pods = v1.list_namespaced_pod(namespace=namespace, watch=False, **kwargs)
+
+    def age_in_hours(pod):
+        """
+        For how many hours has this pod been stuck in the last condition?
+        Return `None` if unknown.
+        """
+        try:
+            transition_times = sorted([cond.last_transition_time for cond in pod.status.conditions])
+            age = datetime.now(timezone.utc) - transition_times[-1]
+            return (age.total_seconds() / 60) / 60
+
+        except:
+            return None
+
+    def predicate(pod):
+        """Decide if we want to delete a pod."""
+        try:
+            name = pod.metadata.name
+
+            if pod_name_prefix is not None and not name.startswith(pod_name_prefix):
+                return False
+
+            if expire_in_hours is None:
+                return True
+
+            age = age_in_hours(pod)
+
+            if age is None:
+                return False
+
+            return (age > expire_in_hours)
+
+        except:
+            # if any of the above assumptions are not quite right, do not delete the pod
+            return False
+
+    def show(pod):
+        """Human-readable pod info."""
+        return '{:} {} {} {:d}h'.format(pod.metadata.namespace,
+                                        pod.metadata.name,
+                                        pod.status.phase,
+                                        int(age_in_hours(pod)))
+
+    for pod in pods.items:
+        if predicate(pod):
+            print(show(pod))
 
 main()
